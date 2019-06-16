db6fc874c7dafad227efa1b0ce643e2d9bbc88b3
davidegirardi <16451191+davidegirardi@users.noreply.github.com>
2019-06-16 17:11:29 +0200

    Make code easier to use in production

--- lua/development_example.lua
+++ lua/development_example.lua
@@ -7,28 +7,31 @@ ardour {
 }
 
 
--- Sub function example
-function another_function()
-	LuaDialog.Message ("another_function",
-                       "Hello from another_function",
-                       LuaDialog.MessageType.Info,
-                       LuaDialog.ButtonType.Close):run()
-end
-
--- Put your main code here
-function main_function()
-	LuaDialog.Message ("main_function",
-                       "Hi, I'm the main function! Let's call another_function .\nShall we?",
-                       LuaDialog.MessageType.Question,
-                       LuaDialog.ButtonType.OK):run()
-    print("")
-    another_function()
-end
-
 -- You MUST have a factory() function in a MixBus script
 function factory()
-    return main_function
+    return function()
+        -- Sub function example
+        function another_function()
+            LuaDialog.Message ("another_function",
+            "Hello from another_function",
+            LuaDialog.MessageType.Info,
+            LuaDialog.ButtonType.Close):run()
+        end
+
+        -- Put your main code here
+        function main_function()
+            LuaDialog.Message ("main_function",
+            "Hi, I'm the main function! Let's call another_function .\nShall we?",
+            LuaDialog.MessageType.Question,
+            LuaDialog.ButtonType.OK):run()
+            another_function()
+        end
+
+        main_function()
+    end
 end
 
 -- Delete this after the testing and before saving the final script
+-- otherwise you will get an "Invalid or missing script-name or script-type"
+-- error
 factory()()
--- lua/test.lua
+++ lua/test.lua
@@ -1,15 +1,11 @@
-ardour {
-    ["type"]    = "EditorAction",
-    name        = "Include LUA",
-    license     = "MIT",
-    author      = "Davide Girardi",
-    description = [[Include another lua script]]
-}
+-- Run this from the Window > Scripting window
+-- It will run the code in my_file
 
 function factory()
     return function()
+        my_file = "/path/to/development_example.lua"
         -- Load the test file into testme
-        test = loadfile("/path/to/development_example.lua")
+        test = loadfile(my_file)
         -- "Run" the file
         test()
     end
