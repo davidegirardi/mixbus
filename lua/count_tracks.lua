163bcb8878c6361e4d96c0227059bddc7780cf03
davidegirardi <16451191+davidegirardi@users.noreply.github.com>
2019-07-31 17:22:14 +0200

    Replace print with a message box

--- lua/count_tracks.lua
+++ lua/count_tracks.lua
@@ -7,11 +7,12 @@ ardour {
 }
 
 function factory () 
-    return function ()
-        local count = 0
-        for t in Session:get_tracks():iter () do
-            count = count + 1
-        end
-        print("Number of tracks in the session:", count)
+    local count = 0
+    for t in Session:get_tracks():iter () do
+        count = count + 1
     end
+    LuaDialog.Message ("Track count",
+                       "Number of tracks in the session:" .. count,
+                       LuaDialog.MessageType.Info,
+                       LuaDialog.ButtonType.Close):run()
 end
